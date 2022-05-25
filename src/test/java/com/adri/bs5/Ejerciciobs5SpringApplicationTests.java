cc5dbe08866254a4e16614b02a986ab2e231ada5
Adrián Tena Burgos <adrian.tena@bosonit.com>
2022-05-25 10:48:14 +0200

    Initial commit

---
+++ src/test/java/com/adri/bs5/Ejerciciobs5SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adri.bs5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejerciciobs5SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
