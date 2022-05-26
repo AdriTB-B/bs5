5f82687d00747a0d3a99b4371693fe5b0cc45d8e
Adrián Tena Burgos <adrian.tena@bosonit.com>
2022-05-26 11:02:23 +0200

    Terminado

--- src/main/java/com/adri/bs5/Aplicacion.java
+++ src/main/java/com/adri/bs5/Aplicacion.java
@@ -5,10 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Aplicacion {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aplicacion.class, args);
+		log.error("Error message");
+		log.warn("Warning message");
+		log.info("Info message");
+		log.debug("Debug message");
+		log.trace("Trace message");
 	}
 
 }
